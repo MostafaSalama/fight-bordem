785d158f3c5a35036ceb8e9c82ce4f85b007a997
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-20 00:26:20 +0200

    update the view to include loading element

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
         <ActivityButton></ActivityButton>
-        <p>Loading ...</p>
+        <p v-if="loading">Loading ...</p>
         <ActivityMessage></ActivityMessage>
     </div>
 </template>
 <script>
+    import  {mapState} from 'vuex'
     import ActivityButton from "../components/ActivityButton";
     import ActivityMessage from "../components/ActivityMessage";
     export default {
         name: 'Activity',
-        components: {ActivityMessage, ActivityButton}
+        components: {ActivityMessage, ActivityButton},
+        computed:mapState(['loading'])
     }
 </script>
