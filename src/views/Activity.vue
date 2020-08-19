ffba449807c6a0ff01236889371a96de972aed7b
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-20 00:09:36 +0200

    create the activity view page

---
+++ src/views/Activity.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ActivityButton></ActivityButton>
+        <p>Loading ...</p>
+        <ActivityMessage></ActivityMessage>
+    </div>
+</template>
+<script>
+    import ActivityButton from "../components/ActivityButton";
+    import ActivityMessage from "../components/ActivityMessage";
+    export default {
+        name: 'Activity',
+        components: {ActivityMessage, ActivityButton}
+    }
+</script>
