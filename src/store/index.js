9037792dc21220c222462c5cbcac3c02bf487748
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-20 00:24:23 +0200

    add loading state to indicate fetching status

--- src/store/index.js
+++ src/store/index.js
@@ -5,20 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    activity : ''
+    activity : '',
+    loading:false
   },
   mutations: {
     setActivity(state,message) {
       console.log(typeof message)
       state.activity = message ;
+    },
+    setLoading(state,value){
+      state.loading =  value ;
     }
   },
   actions: {
     async getActivity(context){
       const API_URL = 'https://www.boredapi.com/api/activity' ;
+      context.commit("setLoading",true) ;
       const response = await fetch(API_URL) ;
       const {activity} = await response.json() ;
       context.commit('setActivity', activity) ;
+      context.commit("setLoading",false);  
     }
   },
   modules: {
