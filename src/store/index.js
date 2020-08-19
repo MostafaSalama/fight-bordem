56c6f2b63deac83a50c1f78d03a045cc47615676
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-20 00:03:02 +0200

    add muation and action to get activity

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    activity : ''
   },
   mutations: {
+    setActivity(state,message) {
+      console.log(typeof message)
+      state.activity = message ;
+    }
   },
   actions: {
+    async getActivity(context){
+      const API_URL = 'https://www.boredapi.com/api/activity' ;
+      const response = await fetch(API_URL) ;
+      const {activity} = await response.json() ;
+      context.commit('setActivity', activity) ;
+    }
   },
   modules: {
   }
