e1cf5ec6eedba7cc4ca81a91b84ac474d3cd0327
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-20 00:20:35 +0200

    create the button to get a new activity

---
+++ src/components/ActivityButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <button class="btn" @click="getActivity">
+        Give Me Something To DO
+    </button>
+</template>
+<script>
+    import {mapActions} from "vuex";
+
+    export default {
+        name : "ActivityButton",
+        methods: {
+            ...mapActions(['getActivity'])
+        }
+    }
+</script>
+<style scoped>
+    .btn {
+        background-color: #371CDB;
+        color :#fff;
+        padding: 10px 18px;
+        cursor: pointer;
+        outline: none;
+        font-weight: 900;
+        border-radius: 4px;
+        transition: 300ms all ease-in-out;
+    }
+    .btn:focus,.btn:active{
+        background-color:#0001CE;
+    }
+    .btn:active {
+        background-color: #9965F4;
+    }
+</style>
