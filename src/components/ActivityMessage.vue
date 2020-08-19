b79bfc2021afcaf50ae8b0c646ed6c9dab15d744
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-20 00:33:51 +0200

    add style to the acitivty message container

--- src/components/ActivityMessage.vue
+++ src/components/ActivityMessage.vue
@@ -1,5 +1,5 @@
 <template>
-        <div>
+        <div class="container">
                 <p>{{activity}}</p>
         </div>
 </template>
@@ -11,3 +11,16 @@
         computed: mapState(['activity']),
     }
 </script>
+<style scoped>
+        .container {
+                text-align: center;
+                margin-top: 20px;
+                font-size: 2.5rem;
+                font-style: italic;
+                font-weight: 900;
+                box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+                padding: 20px;
+                word-break: break-word;
+        }
+       
+</style>
