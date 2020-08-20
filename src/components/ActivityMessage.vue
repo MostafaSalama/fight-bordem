6d273ecf4cc1f784c7cc0beb7cce68fecb1c6093
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-20 12:33:53 +0200

    add heart icon events

--- src/components/ActivityMessage.vue
+++ src/components/ActivityMessage.vue
@@ -1,26 +1,39 @@
 <template>
-        <div class="container">
-                <p>{{activity}}</p>
-        </div>
+	<div class="container">
+		<p>{{ activity }}</p>
+		<HeartIcon @update-liked="updateLiked" :fill-color="fillColor"></HeartIcon>
+	</div>
 </template>
 <script>
-        import {mapGetters} from "vuex";
+import { mapGetters, mapState } from 'vuex';
+import HeartIcon from './HeartIcon';
 
-    export default  {
-        name : 'ActivityMessage',
-        computed: mapGetters(['activity']),
-    }
+export default {
+	name: 'ActivityMessage',
+	components: { HeartIcon },
+	computed: {
+		...mapGetters(['activity']),
+		...mapState(['isCurrentActivityLiked']),
+		fillColor() {
+			return this.isCurrentActivityLiked ? '#E0245E' : '#f5efef';
+		},
+	},
+	methods: {
+		updateLiked() {
+			this.$store.dispatch('setLiked',!this.isCurrentActivityLiked);
+		},
+	},
+};
 </script>
 <style scoped>
-        .container {
-                text-align: center;
-                margin-top: 20px;
-                font-size: 2.5rem;
-                font-style: italic;
-                font-weight: 900;
-                box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-                padding: 20px;
-                word-break: break-word;
-        }
-
+.container {
+	text-align: center;
+	margin-top: 20px;
+	font-size: 2.5rem;
+	font-style: italic;
+	font-weight: 900;
+	box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+	padding: 20px;
+	word-break: break-word;
+}
 </style>
