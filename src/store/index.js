4cf16caeacb8783faea488f12a48e33e88d2b014
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-20 12:51:22 +0200

    add methods to remove and toggle activities

--- src/store/index.js
+++ src/store/index.js
@@ -1,45 +1,57 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import {nanoid} from 'nanoid'
-Vue.use(Vuex)
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { nanoid } from 'nanoid';
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    activity : {},
-    loading:false,
-    isCurrentActivityLiked:false,
-    favoriteActivities:[]
-  },
-  getters:{
-    activity(state){
-      return state.activity.activity ;
-    }
-  },
-  mutations: {
-    setActivity(state,payload) {
-      state.activity = payload ;
-    },
-    setLiked(state,value) {
-        state.isCurrentActivityLiked = value ;
-        if(state.isCurrentActivityLiked) {
-          state.favoriteActivities.unshift(state.activity) ;
-        }
-    },
-    setLoading(state,value){
-      state.loading =  value ;
-    }
-  },
-  actions: {
-    async getActivity(context){
-      const API_URL = 'https://www.boredapi.com/api/activity' ;
-      context.commit("setLoading",true) ;
-      const response = await fetch(API_URL) ;
-      const {activity} = await response.json() ;
-      context.commit('setActivity', {activity,id:nanoid()}) ;
-      context.commit('setLiked',false);
-      context.commit("setLoading",false);
-    }
-  },
-  modules: {
-  }
-})
+	state: {
+		activity: {},
+		loading: false,
+		isCurrentActivityLiked: false,
+		favoriteActivities: [],
+	},
+	getters: {
+		activity(state) {
+			return state.activity.activity;
+		},
+	},
+	mutations: {
+		setActivity(state, payload) {
+			state.activity = payload;
+		},
+		setLiked(state, value) {
+			state.isCurrentActivityLiked = value;
+		},
+		addActivity(state, activity) {
+			state.favoriteActivities = [activity, ...state.favoriteActivities];
+		},
+		setLoading(state, value) {
+			state.loading = value;
+		},
+		removeActivity(state, activityId) {
+			state.favoriteActivities = state.favoriteActivities.filter(
+				(a) => a.id !== activityId,
+			);
+		},
+	},
+	actions: {
+		async getActivity(context) {
+			const API_URL = 'https://www.boredapi.com/api/activity';
+			context.commit('setLoading', true);
+			const response = await fetch(API_URL);
+			const { activity } = await response.json();
+			context.commit('setActivity', { activity, id: nanoid() });
+			context.commit('setLiked', false);
+			context.commit('setLoading', false);
+		},
+		setLiked(context, value) {
+			if (value) {
+				context.commit('addActivity', context.state.activity);
+			} else {
+				context.commit('removeActivity', context.state.activity.id);
+			}
+			context.commit('setLiked', value);
+		},
+	},
+	modules: {},
+});
